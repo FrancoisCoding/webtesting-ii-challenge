43094aef615ec88972810a0b13650febce91e8d4
Isaiah Francois <francoisisaiaht@yahoo.com>
2019-08-08 11:38:17 -0400

    Created and rendered to screen Display and Dashboard component

--- webtesting-ii-challenge/src/App.js
+++ webtesting-ii-challenge/src/App.js
@@ -1,10 +1,14 @@
 import React from "react";
 import "./App.css";
+import Display from "./components/Display";
+import Dashboard from "./components/Dashboard";
 
 function App() {
   return (
     <div className="App">
       <h1>App</h1>
+      <Display />
+      <Dashboard />
     </div>
   );
 }
