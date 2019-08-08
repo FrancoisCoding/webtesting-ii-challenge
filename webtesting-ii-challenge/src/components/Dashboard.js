2ccec1527897f3ce189c436a8f241b6e6f52f01b
Isaiah Francois <francoisisaiaht@yahoo.com>
2019-08-08 12:00:57 -0400

    Created logic for ball button

--- webtesting-ii-challenge/src/components/Dashboard.js
+++ webtesting-ii-challenge/src/components/Dashboard.js
@@ -4,11 +4,18 @@ import Display from "./Display";
 export default function Dashboard() {
   const [balls, setBalls] = useState(0);
   const [strikes, setStrikes] = useState(0);
+  const ballsIncrement = () => {
+    if (balls < 4) {
+      setBalls(balls + 1);
+    } else {
+      setBalls(0);
+    }
+  };
   return (
     <div>
       <h1>Dashboard</h1>
       <button>Strike</button>
-      <button>Ball</button>
+      <button onClick={() => ballsIncrement()}>Ball</button>
       <button>Foul</button>
       <button>Hit</button>
       <Display
